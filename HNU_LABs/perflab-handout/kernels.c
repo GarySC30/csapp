4980d85ff9c2ac4d615ce5f3bfbd48f680ae46b1
gary <garygaogsc@outlook.com>
2021-04-06 13:33:58 +0800

    增加其他rotate函数

--- HNU_LABs/perflab-handout/kernels.c
+++ HNU_LABs/perflab-handout/kernels.c
@@ -44,8 +44,20 @@ void naive_rotate(int dim, pixel *src, pixel *dst)
  * rotate - Your current working version of rotate
  * IMPORTANT: This is the version you will be graded on
  */
-char rotate_descr[] = "rotate: Current working version";
-void rotate(int dim, pixel *src, pixel *dst) 
+char rotate_descr_1[] = "rotate1: Current working version";
+void rotate1(int dim, pixel *src, pixel *dst) 
+{
+    int i, j;
+
+    for (i = 0; i < dim; i++)
+	for (j = 0; j < dim; j++) {
+        int tmp = dim -1 -j;  // 将dim-i-1修改为独立变量 消除循环的低效率
+	    dst[RIDX(tmp, i, dim)] = src[RIDX(i, j, dim)];
+    }
+}
+
+char rotate_descr_2[] = "rotate2: Current working version";
+void rotate2(int dim, pixel *src, pixel *dst) 
 {
     int i, j;
 
@@ -67,7 +79,8 @@ void rotate(int dim, pixel *src, pixel *dst)
 void register_rotate_functions() 
 {
     add_rotate_function(&naive_rotate, naive_rotate_descr);   
-    add_rotate_function(&rotate, rotate_descr);   
+    add_rotate_function(&rotate1, rotate_descr_1);
+    add_rotate_function(&rotate2, rotate_descr_2);   
     /* ... Register additional test functions here */
 }
 
